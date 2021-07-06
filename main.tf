eb28ab98a031b7616f5abd54053b2f28429ea5d6
shruthi <shruthi.setty@outlook.com>
2021-07-06 13:23:32 -0700

    updating files

--- main.tf
+++ main.tf
@@ -29,7 +29,7 @@ resource "azurerm_subnet" "main" {
   name                 = "${var.prefix}-subnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.2.0/24"]
+  address_prefix     = ["10.0.2.0/24"]
   
 }
 
